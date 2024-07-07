3c124f84ce4a01d16e558b39ea277f98eeb1788f
Yogesh <yogeshraj5971@gmail.com>
2024-07-07 19:59:24 +0530

    All Files added

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -2,7 +2,7 @@ package ex_07072024;
 
 public class Lab001 {
     public static void main(String[] args) {
-        System.out.println("Yogesh Hello");
+        System.out.println("Hello");
     }
 
 
