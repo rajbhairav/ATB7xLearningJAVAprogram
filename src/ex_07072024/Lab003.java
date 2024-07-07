068bf212a386dcf497655ef6bdfde6a2b202ac84
Yogesh <yogeshraj5971@gmail.com>
2024-07-07 20:11:39 +0530

    All Files added

--- src/ex_07072024/Lab003.java
+++ src/ex_07072024/Lab003.java
@@ -2,16 +2,15 @@ package ex_07072024;
 
 public class Lab003 {
     public static void main(String[] args) {
-        // Single comment - This code will not run or executed
-        // 2+2 ?
-        // Purpose Comment ?
-        // Instrutions to Jr QA or Dev
-        // Explain
-        /**
-         * Multiple Comment
-         * Documentation Purpose
-         *
-         *
-         */
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(3/2);
+
+
+
+
     }
 }
